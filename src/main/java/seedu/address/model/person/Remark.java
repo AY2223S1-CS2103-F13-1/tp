0b1ec91851dff09d226152fcc53318d309af817f
ConradLew <e0726724@u.nus.edu>
2022-09-26 02:23:04 +0800

    Fix javadoc headers and import order

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructor for a remark
+     *
+     * @param remark String representing the remark.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
